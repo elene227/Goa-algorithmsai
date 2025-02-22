bdf77aa635af3c283f9de5bc6dca5d9c08a95675
elene227 <elelopirtskhalava@gmail.>
2025-02-22 12:51:37 +0400

    commit

--- Day 07/homework.py
+++ Day 07/homework.py
@@ -1,23 +1,10 @@
-def convert_number(number, from_base, to_base):
+binary = [29, 1, 14, 25, 15, 21, 18, 5, 1, 4, 20, 8, 9, 19, 53, 35, 6, 25, 15, 21, 3, 1, 14, 18, 5, 1, 4, 20, 8, 9, 19, 55, 20, 8, 1, 20, 13, 5, 1, 14, 19, 25, 15, 21, 1, 12, 18, 5, 1, 4, 25]
 
-    decimal_number = int(number, from_base)
+user_input = input("Please type 'yes' to print translation: ")
 
-
-    if to_base == 10:
-        return decimal_number
-    
-
-    result = ""
-    while decimal_number > 0:
-        result = str(decimal_number % to_base) 
-        decimal_number = decimal_number // to_base
-
-    return result
-                                                                # ათწლიანში გადაყვანა
-number = input("რიცხვი: ")
-from_base = int(input("საწყისი ბაზა: "))
-to_base = int(input("მიზნობრიივ ბაზა: ")) # გადაქცევა რიცხვის რა ბაზაშიც გვინდა 
-
-convert_number = (number, from_base, to_base)
-
-print (convert_number)
+if user_input.lower() == "yes":
+    print(binary)
+elif user_input.lower() == "no":
+    print("Error: You typed no.")
+else:
+    print("Invalid input, please type 'yes' or 'no'.")
